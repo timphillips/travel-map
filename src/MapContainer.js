802e5d3c3215e2e92d5175a14484af984489a5c1
Tim Phillips <timdphillips@gmail.com>
2018-03-03 14:40:15 +0000

    Add footer to sidebar

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,34 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 import { Map, Marker } from "google-maps-react";
 
-export class MapContainer extends Component {
-  render() {
-    const locations = this.props.locations;
-    const locationMarkers = locations.map(location => (
-      <Marker
-        key={location.id}
-        title={location.name}
-        name={location.name}
-        position={{
-          lat: location.position.latitude,
-          lng: location.position.longitude
-        }}
-      />
-    ));
+export function MapContainer(props) {
+  const locationMarkers = props.locations.map(location => (
+    <Marker
+      key={location.id}
+      title={location.name}
+      name={location.name}
+      position={{
+        lat: location.position.latitude,
+        lng: location.position.longitude
+      }}
+    />
+  ));
 
-    return (
-      <div className="mapContainer">
-        <Map
-          google={this.props.google}
-          zoom={3}
-          initialCenter={{
-            lat: 35,
-            lng: -35
-          }}
-        >
-          {locationMarkers}
-        </Map>
-      </div>
-    );
-  }
+  return (
+    <div className="mapContainer">
+      <Map
+        google={props.google}
+        zoom={3}
+        initialCenter={{
+          lat: 35,
+          lng: -35
+        }}
+      >
+        {locationMarkers}
+      </Map>
+    </div>
+  );
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { ExpandableList } from "./ExpandableList";
 
 function compareStrings(a, b) {
@@ -11,33 +11,50 @@ function compareStrings(a, b) {
   return 0;
 }
 
-export class Sidebar extends Component {
-  render() {
-    const locations = this.props.locations
-      .sort((a, b) => compareStrings(a.name, b.name))
-      .map(location => `${location.name}, ${location.country}`);
+export function Sidebar(props) {
+  const locations = props.locations
+    .sort((a, b) => compareStrings(a.name, b.name))
+    .map(location => `${location.name}, ${location.country}`);
 
-    const countries = Array.from(
-      // use "Set" to obtain distinct set of countries
-      new Set(
-        this.props.locations
-          .map(location => location.country)
-          .sort(compareStrings)
-      )
-    );
+  const countries = Array.from(
+    // use "Set" to obtain distinct set of countries
+    new Set(
+      props.locations.map(location => location.country).sort(compareStrings)
+    )
+  );
 
-    return (
-      <aside className="sidebar">
-        <div className="sidebar__header">
-          <h3 className="sidebar__headerTitle">Travel Log</h3>
-          <p className="sidebar__headerSubtitle">
-            My adventures around the world...
-          </p>
-        </div>
+  return (
+    <aside className="sidebar">
+      <div className="sidebar__header">
+        <h3 className="sidebar__headerTitle">Travel Log</h3>
+      </div>
 
+      <div className="sidebar__controls">
         <ExpandableList title="Locations" items={locations} />
         <ExpandableList title="Countries" items={countries} />
-      </aside>
-    );
-  }
+      </div>
+
+      <div className="sidebar__footer">
+        <a
+          className="sidebar__openSource"
+          href="https://github.com/timphillips/travel-map"
+          target="_blank"
+        >
+          <img
+            className="sidebar__openSourceLogo"
+            src="github-32px.png"
+            width={20}
+            height={20}
+          />
+          <span className="sidebar__openSourceLabel">Open Source</span>
+        </a>
+        <span>
+          Built by{" "}
+          <a href="http://www.tim-phillips.com" target="_blank">
+            Tim Phillips
+          </a>
+        </span>
+      </div>
+    </aside>
+  );
 }
